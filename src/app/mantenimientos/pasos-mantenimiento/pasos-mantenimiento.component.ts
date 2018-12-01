0ff062a157cb328206734d0d042f0d61c519b96e
marisolgarcia <egarcia.abrego@gmail.com>
2018-12-01 14:15:42 -0600

    cambios

--- src/app/mantenimientos/pasos-mantenimiento/pasos-mantenimiento.component.ts
+++ src/app/mantenimientos/pasos-mantenimiento/pasos-mantenimiento.component.ts
@@ -5,7 +5,9 @@ import { User } from '../../clases/user';
 import { Subscription } from 'rxjs';
 import { GlobalService } from '../../servicios/global.service';
 import { Paso } from '../../clases/paso';
+import { Procedimiento } from '../../clases/procedimiento';
 import { MantenimientoPasosService } from '../../servicios/mantenimiento-pasos.service';
+import { MantenimientoProcedimientosService } from '../../servicios/mantenimiento-procedimientos.service';
 
 @Component({
   selector: 'app-pasos-mantenimiento',
@@ -16,6 +18,7 @@ export class PasosMantenimientoComponent implements OnInit {
 
   displayedColumns = ['number', 'nombre', 'descripcion', 'procedimiento', 'orden', /*'opcion'*/];
   pasos: Paso[];
+  procedimientos: Procedimiento[];
   dataSource = new MatTableDataSource();
   account: User = new User();
   userSub: Subscription;
@@ -23,7 +26,8 @@ export class PasosMantenimientoComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private pasoService : MantenimientoPasosService, private _router:Router, private global: GlobalService) { 
+  constructor(private pasoService : MantenimientoPasosService, private _router:Router, private global: GlobalService, 
+    private procedimientoService : MantenimientoProcedimientosService) { 
 
   }
 
@@ -33,17 +37,28 @@ export class PasosMantenimientoComponent implements OnInit {
     //Verificar el logueo
     if (localStorage.getItem('token') && localStorage.getItem('account')){     // 
       this.global.me = JSON.parse(localStorage.getItem('account'));
-      this.getProcedimientos();
+      this.getPaso();
+      this.getProcedimimentos();
   } else {
       this._router.navigate(['/home']);
   }
 }
-  getProcedimientos(){
+
+  getPaso(){
     this.pasoService.getPasos().subscribe(pasos =>{
       this.dataSource.data = pasos;
       this.ngAfterViewInit();
       this.pasos = this.pasos;
       console.log('pasos', pasos);
+    }, error =>{
+      console.log('error', error);
+    })
+  }
+
+  getProcedimimentos(){
+    this.procedimientoService.getProcedimiento().subscribe(procedimentos =>{
+      this.procedimientos = procedimentos;
+      console.log('procedimientos', procedimentos);
     }, error =>{
       console.log('error', error);
     })
@@ -63,4 +78,7 @@ export class PasosMantenimientoComponent implements OnInit {
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
+
+  
+
 }
